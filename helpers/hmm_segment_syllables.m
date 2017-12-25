c61cb3a42d67f87fe3a47dbb5ec461c8b4b09d93
yardencsGitHub <yardenc@bu.edu>
2017-12-25 16:38:15 -0500

    segment

--- helpers/hmm_segment_syllables.m
+++ helpers/hmm_segment_syllables.m
@@ -6,19 +6,31 @@ function tot = hmm_segment_syllables(s,labels,fbins)
     dlabels = find(diff([0 labels 0]) ~= 0);
     tot = zeros(1,size(s,2));
     for loc = 1:numel(dlabels)-1
-        sig = sum(s(fbins(1):fbins(2),dlabels(loc):dlabels(loc+1)-1)).^(1/3);
-        sig = sig-min(sig);
-        sig = sig+randn(size(sig))*1e-3;
-        [model, loglikHist] = hmmFit(sig, nstates,'student');             
-        map_path = hmmMap(model, sig)-1;
-        if mean(sig(map_path==1)) < mean(sig(map_path==0))
-            map_path = 1-map_path;
+        if (labels(dlabels(loc)) ~= 0)
+            warncnt = 0; 
+            lastwarn('');
+            while warncnt < 10
+                sig = sum(s(fbins(1):fbins(2),dlabels(loc):dlabels(loc+1)-1)).^(1/3);
+                sig = sig-min(sig);
+                sig = sig+randn(size(sig))*1e-2;
+                [model, loglikHist] = hmmFit(sig, nstates,'student');             
+                map_path = hmmMap(model, sig)-1;
+                if mean(sig(map_path==1)) < mean(sig(map_path==0))
+                    map_path = 1-map_path;
+                end
+                if ~any(map_path == 1)
+                    map_path = ones(size(map_path));
+                end
+                [msgstr, msgid] = lastwarn;
+                if isempty(msgid)
+                    break;
+                else
+                    warncnt = warncnt + 1;
+                end
+            end
+
+            tot(dlabels(loc):dlabels(loc+1)-1) = map_path;
         end
-        if ~any(map_path == 1)
-            map_path = ones(size(map_path));
-        end
-   
-        tot(dlabels(loc):dlabels(loc+1)-1) = map_path;
 
     end
 
