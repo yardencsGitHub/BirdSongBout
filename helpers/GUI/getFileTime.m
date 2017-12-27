d9b05a8799006184507e3d5aca34123dd0bc4af6
yardencsGitHub <yardenc@bu.edu>
2017-12-27 16:17:35 -0500

    updates
    including a new fun to create ‘templates’ and populates the ‘elements’
    from mat files with labels

---
+++ helpers/GUI/getFileTime.m
@@ -0,0 +1,19 @@
+function time = getFileTime(filename)
+if strcmp(filename(end-2:end),'mat')
+    filename = filename(1:end-4);
+end
+strparts = regexp(filename,'_', 'split');
+
+y = str2double(strparts{3});
+m = str2double(strparts{4});
+d = str2double(strparts{5});
+th = str2double(strparts{6});
+tm = str2double(strparts{7});
+try
+    ts = strparts{8};
+    ts =  str2double(ts(1:end-4));
+catch em
+    ts = 0;
+end
+
+time = datenum(y,m,d,th,tm,ts);
