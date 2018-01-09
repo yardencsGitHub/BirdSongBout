686325737ac1722faea8eb957c93e13e34236644
yardencsGitHub <yardenc@bu.edu>
2018-01-09 18:18:13 -0500

    updates

--- CanaryBoutPipeLine.m
+++ CanaryBoutPipeLine.m
@@ -6,15 +6,15 @@
 % 3.  In the wav file directory: [keys, elements, templates] = create_empty_elements(DIR,bird_exper_name,[])
 % 4. [keys, elements, templates] = populate_existing_elements(path_to_annotation_file,path_to_mat_files,path_to_wav_files,dt)
 %%
-cd('/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_4');
-path_to_annotation_file = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_4/bird4_annotation.mat';
-path_to_mat_files = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_4/mat';
-path_to_wav_files = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_4';
-[keys, elements, templates] = create_empty_elements(pwd,'bird4',[]);
-save bird4_annotation keys elements;
-save bird4_templates templates;
+cd('/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_6');
+path_to_annotation_file = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_6/bird6_annotation.mat';
+path_to_mat_files = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_6/mat';
+path_to_wav_files = '/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_6';
+[keys, elements, templates] = create_empty_elements(pwd,'bird6',[]);
+save bird6_annotation keys elements;
+save bird6_templates templates;
 dt = 1/3.692307692307692e+02;
 [keys, elements, templates] = populate_existing_elements(path_to_annotation_file,path_to_mat_files,path_to_wav_files,dt);
-cd('/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_4');
-save bird4_annotation keys elements;
-save bird4_templates templates;
+cd('/Users/yardenc/Documents/Experiments/CanaryBoutAnnotation/bird_6');
+save bird6_annotation keys elements;
+save bird6_templates templates;
