8cfbdebb5fe205bfc8782ff6d641c31cec10578b
yardencsGitHub <yardenc@bu.edu>
2018-11-19 11:03:47 -0500

    more scripts

---
+++ helpers/Add_outliers_to_annotation.m
@@ -0,0 +1,42 @@
+%%
+% This script takes data structure of outliers matched to the structured
+% created by 'Prepare_individual_syllable_spects.m' and created by
+% 'FindOutliers.ipynb'
+% The script needs the matching annotation and template files and will
+% create a ne version of those with annotated outliers appended with '00'
+
+%% 
+work_folder = '/Users/yardenc/Documents/Experiments/Behavior/Data/CanaryData/Chondroitinase/llb11/Individual syllable spects/';
+orig_annotation = '/Users/yardenc/Documents/Experiments/Behavior/Data/CanaryData/Chondroitinase/llb11/llb11_annotation_Vika_initial_part_1.mat';
+orig_template = '/Users/yardenc/Documents/Experiments/Behavior/Data/CanaryData/Chondroitinase/llb11/llb11_templates_Vika_initial.mat';
+cd(work_folder);
+outlier_prefix = 'output_syllable_spects_';
+spect_prefix = 'syllable_spects_';
+num_syls = 29;
+%% fix templates
+load(orig_template)
+templates.wavs = [templates.wavs templates.wavs];
+for cnt = num_syls+1:2*num_syls
+    templates.wavs(cnt).segType = templates.wavs(cnt).segType*100;
+end
+
+[DIR,filename,ext]=fileparts(orig_template);
+new_template_file = fullfile(DIR,['outliers_' filename ext]);
+save(new_template_file,'templates');
+%% fix annotation
+load(orig_annotation);
+for cnt = 1:num_syls
+    load([spect_prefix num2str(cnt) '.mat']);
+    for segnum = 1:size(syllable_spects.idx,1)
+        fnum = syllable_spects.idx(segnum,1);
+        loc = syllable_spects.idx(segnum,2);
+        elements{fnum}.segType(loc) = elements{fnum}.segType(loc)*100;
+    end
+end
+
+[DIR,filename,ext]=fileparts(orig_annotation);
+new_annotation_file = fullfile(DIR,['outliers_' filename ext]);
+save(new_annotation_file,'keys','elements');
+
+disp('done')
+
---
+++ helpers/Prepare_individual_syllable_spects.m
@@ -0,0 +1,84 @@
+function Prepare_individual_syllable_spects(path_to_annotation,path_to_audio,path_to_target)
+% This script prepares spectrogram snippets of all syllables and creates
+% files for those syllable classes.
+%   Prepare_individual_syllable_spects(path_to_annotation,path_to_audio)
+%   Inputs are the paths
+%
+%%
+ignore_syllables = -1;
+TargetDir = path_to_target;
+
+%% organize and find syllable classes
+cd(path_to_audio);
+load(path_to_annotation);
+ords = [];
+dates = [];
+syl_classes = [];
+for fnum = 1:numel(keys)
+    tokens = regexp(keys{fnum},'_','split');
+    ords(fnum) = str2num(tokens{2});
+    dates = [dates; datenum(char(join(tokens(3:5),'_')))];
+    syl_classes = unique(union(syl_classes,setdiff(unique(elements{fnum}.segType),ignore_syllables)));
+end
+[locs,indx] = sort(ords);
+elements = elements(indx);
+keys = keys(indx);
+dates = dates(indx,:);
+%% count syllables
+n_syl = numel(syl_classes);
+flag = 1;
+syl_counts = zeros(n_syl,1);
+hf = waitbar(0,'counting syllables');
+for fnum = 1:numel(keys)
+    tokens = regexp(keys{fnum},'_','split');
+    for segnum = 1:numel(elements{fnum}.segType)
+        if ismember(elements{fnum}.segType(segnum),syl_classes)
+            syloc = find(syl_classes == elements{fnum}.segType(segnum));
+            syl_counts(syloc) = syl_counts(syloc) + 1; 
+        end
+    end
+    waitbar(fnum/numel(keys),hf);
+end
+close(hf);
+
+%%
+
+for syltype = 1:n_syl
+    clear syllable_spects;
+    sylnum = syl_classes(syltype);
+    
+    syllable_spects.syl_num = sylnum;
+    syllable_spects.samples = cell(syl_counts(syltype),1);
+    syllable_spects.idx = zeros(syl_counts(syltype),2);
+    syl_cnt = 1;
+    flag = 1;
+    clc;
+    hf = waitbar(0,['Creating file ' num2str(syltype) ' / ' num2str(n_syl)]);
+    for filenum = 1:numel(elements)
+        waitbar(filenum/numel(keys),hf);
+        fname = keys{filenum};
+        loc = filenum;
+        %phrases = return_phrase_times(elements{loc});
+        
+        [y,fs] = audioread([fname(1:end-3) 'wav']);
+        [S,F,T,P] = spectrogram((y/(sqrt(mean(y.^2)))),440,440-88,1024,fs); %440,440-88
+        
+        for segnum = 1:numel(elements{loc}.segType)
+            if (elements{loc}.segType(segnum) == sylnum)
+                tidx = find(T >= elements{loc}.segFileStartTimes(segnum) & T <= elements{loc}.segFileEndTimes(segnum));
+                syloc = find(syl_classes == elements{loc}.segType(segnum));
+                syllable_spects.samples{syl_cnt} = single(log(1+abs(S(:,tidx))));
+                syllable_spects.idx(syl_cnt,:) = [filenum segnum];
+                syl_cnt = syl_cnt + 1;
+%                 if syl_cnt > 2000
+%                     flag = 0;
+%                 end
+            end
+        end
+        %clc;
+        %disp([filenum numel(elements)]);
+    end
+    outfile = fullfile(TargetDir,['syllable_spects_' num2str(sylnum) '.mat']);
+    save(outfile,'syllable_spects','-v6');
+    close(hf);
+end
