a4645b6cbf4c00162d4d38f1e352b8e3c0f9465c
Yarden Cohen <yardencsmail@gmail.com>
2024-11-04 11:33:02 +0200

    update formatting

--- helpers/bsb_extrat_date_string_trom_filename.m
+++ helpers/bsb_extrat_date_string_trom_filename.m
@@ -1,4 +1,4 @@
-function [datename, dateobj] = bsb_extrat_date_string_trom_filename(filename,filename_expression)
+function [datename, dateobj] = bsb_extrat_date_string_trom_filename(filename,filename_expression,varargin)
 % This function allows extracting the date and time encoded in file names.
 
 % Inputs:
@@ -17,62 +17,89 @@ function [datename, dateobj] = bsb_extrat_date_string_trom_filename(filename,fil
 %                      (e.g. 2018-05-04-12-51-23 for the above example)
 %   dateobj (datetime object): Matlab's datetime object for above details
 
-        year_idx = regexp(filename_expression,'yyyy');
-        if numel(year_idx) ~= 1
-            disp('error in year formatting');
-            datename = [];
-            return;
-        else 
-            year_idx = year_idx:(year_idx+3);
-        end
-    
-        month_idx = regexp(filename_expression,'MM');
-        if numel(month_idx) ~= 1
-            disp('error in month formatting');
-            datename = [];
-            return;
-        else 
-            month_idx = month_idx:(month_idx+1);
-        end
-    
-        day_idx = regexp(filename_expression,'dd');
-        if numel(day_idx) ~= 1
-            disp('error in day formatting');
-            datename = [];
-            return;
-        else 
-            day_idx = day_idx:(day_idx+1);
-        end
-    
-        hour_idx = regexp(filename_expression,'HH');
-        if numel(hour_idx) ~= 1
-            disp('error in hour formatting');
-            datename = [];
-            return;
-        else 
-            hour_idx = hour_idx:(hour_idx+1);
-        end
-    
-        minute_idx = regexp(filename_expression,'mm');
-        if numel(minute_idx) ~= 1
-            disp('error in minute formatting');
-            datename = [];
-            return;
-        else 
-            minute_idx = minute_idx:(minute_idx+1);
-        end
-    
-        second_idx = regexp(filename_expression,'ss');
-        if numel(second_idx) ~= 1
-            disp('error in second formatting');
-            datename = [];
-            return;
-        else 
-            second_idx = second_idx:(second_idx+1);
-        end
-
-        date_string = [filename([year_idx month_idx day_idx hour_idx minute_idx second_idx])];
-        dateobj = datetime(date_string,'InputFormat','yyyyMMddHHmmss');
-
-        datename = datestr(dateobj,'yyyy-mm-dd-HH-MM-ss');
+year_prefix = '20'; % to change in 2030
+minute_place = 1; % in case the file name has more than one occurance of 'mm'
+nparams=length(varargin);
+if mod(nparams,2)>0
+    error('Screening:argChk','Parameters must be specified as parameter/value pairs!');
+end
+for i=1:2:nparams
+    switch lower(varargin{i})
+        case 'year_prefix'
+            year_prefix=varargin{i+1};
+        case 'minute_place'
+            minute_place=varargin{i+1};
     end
+end
+year_idx = regexp(filename_expression,'yyyy');
+if isempty(year_idx)
+    year_idx = regexp(filename_expression,'yy');
+    if numel(year_idx) ~= 1
+        disp('error in year formatting');
+        datename = [];
+        return;
+    else 
+        year_idx = year_idx:(year_idx+1);
+    end
+else
+    year_prefix = [];
+    if numel(year_idx) ~= 1
+        disp('error in year formatting');
+        datename = [];
+        return;
+    else 
+        year_idx = year_idx:(year_idx+3);
+    end
+end
+month_idx = regexp(filename_expression,'MM');
+if numel(month_idx) ~= 1
+    disp('error in month formatting');
+    datename = [];
+    return;
+else 
+    month_idx = month_idx:(month_idx+1);
+end
+
+day_idx = regexp(filename_expression,'dd');
+if numel(day_idx) ~= 1
+    disp('error in day formatting');
+    datename = [];
+    return;
+else 
+    day_idx = day_idx:(day_idx+1);
+end
+
+hour_idx = regexp(filename_expression,'HH');
+if numel(hour_idx) ~= 1
+    disp('error in hour formatting');
+    datename = [];
+    return;
+else 
+    hour_idx = hour_idx:(hour_idx+1);
+end
+
+minute_idx = regexp(filename_expression,'mm');
+if numel(minute_idx) ~= 1
+    disp('Multiple mm in file name. Using place input');
+    minute_idx = minute_idx(minute_place);
+    minute_idx = minute_idx:(minute_idx+1);
+    % datename = [];
+    % return;
+else 
+    minute_idx = minute_idx:(minute_idx+1);
+end
+
+second_idx = regexp(filename_expression,'ss');
+if numel(second_idx) ~= 1
+    disp('error in second formatting');
+    datename = [];
+    return;
+else 
+    second_idx = second_idx:(second_idx+1);
+end
+
+date_string = [year_prefix filename([year_idx month_idx day_idx hour_idx minute_idx second_idx])];
+dateobj = datetime(date_string,'InputFormat','yyyyMMddHHmmss');
+
+datename = datestr(dateobj,'yyyy-mm-dd-HH-MM-ss');
+end
